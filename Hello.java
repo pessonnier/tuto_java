21952bdcd59a6046ecaf8d2ca93ad1300cea307c
loic.pessonnier@gmail.com <loic.pessonnier@gmail.com>
2019-04-02 19:45:25 +0200

    etape 2

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        String x = "je m'appel Kenshin";
+        String z = "je m'appel ";
+        String y = "je ne m'appel pas "; 
+        int a = 2;
+        int b = 4;
+        int c = a+b;
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(z+args[0]);
+        System.out.println(y+args[1]);
+    }
+}
